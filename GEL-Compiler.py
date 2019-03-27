f6529a43e73499c8d69864d992b6a320ba4f95fc
LEGOJACKI <jack.gs.code@gmail.com>
2019-03-27 14:37:54 -0400

    General improvements, horizontal gradient support
    Going to add in vertical and circular gradients next. Currently crashes with gradients of more than 255 steps and won't reach the end color with less than 255 steps. Can be worked around with scaling for now

--- GEL-Compiler.py
+++ GEL-Compiler.py
@@ -4,8 +4,8 @@ from zipfile import *
 pygame.init()
 
 filename = input("Enter filename: ")
-scalar = int(input("Enter scalar value, or one for no scaling"))
-screen = pygame.display.set_mode((int(input("Enter Width: "))*scalar, int(input("Enter Height: "))*scalar))
+scalar = float(input("Enter scalar value, or one for no scaling"))
+
 text = "CIRCLE: 0, 0, 20"
 if filename.split('.')[1] == "txt":
   fh = open(filename, 'r')
@@ -20,11 +20,18 @@ elif filename.split('.')[1] == "zip":
   textlines = text.decode("utf-8").split("\r\n")
 
 currentColor = pygame.Color(255, 255, 255)
-pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(0, 0, screen.get_width(), screen.get_height()))
+
 for i in range(0, len(textlines)):
+  pygame.event.get()
   line = textlines[i]
   if not line == "":
-      if line[0:4] == "RGB:":
+      if line[0:5] == "SIZE:":
+        datavals = line.replace("SIZE:", "").replace(" ", "").split(",")
+        width = int(datavals[0]) * scalar
+        height = int(datavals[1]) * scalar
+        screen = pygame.display.set_mode((int(width), int(height)))
+        pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(0, 0, screen.get_width(), screen.get_height()))
+      elif line[0:4] == "RGB:":
         datavals = line.replace(" ", "").replace("RGB:", "").split(",")
         currentColor.r = int(datavals[0])
         currentColor.g = int(datavals[1])
@@ -43,17 +50,86 @@ for i in range(0, len(textlines)):
         width = int(datavals[2])
         height = int(datavals[3])
         pygame.draw.rect(screen, currentColor, pygame.Rect(x*scalar, y*scalar, width*scalar, height*scalar))
+      elif line[0:5] == "GRAD:":
+        cleanline = line.replace("(", "").replace(")", "").replace(" ", "").replace("x", ",").replace("GRAD:", "")
+        datavals = cleanline.split(",")
+        x = int(datavals[0])
+        y = int(datavals[1])
+        width = int(datavals[2])
+        height = int(datavals[3])
+        direction = datavals[4]
+        endColor = pygame.Color(255, 255, 255)
+        gradColor = pygame.Color(currentColor.r, currentColor.g, currentColor.b)
+        endColor.r = int(datavals[5])
+        endColor.g = int(datavals[6])
+        endColor.b = int(datavals[7])
+        try:
+          endColor.a = int(datavals[8])
+          if direction == 'side':
+            if endColor.r > currentColor.r:
+              diffred = currentColor.r - endColor.r
+            else:
+              diffred = currentColor.r - endColor.r
+            
+            if endColor.g > currentColor.g:
+              diffgreen = currentColor.g - endColor.g
+            else:
+              diffgreen = currentColor.g - endColor.g
+            
+            if endColor.b > currentColor.b:
+              diffblue = currentColor.b - endColor.b
+            else:
+              diffblue = currentColor.b - endColor.b
+            
+            diffalpha = abs(currentColor.a - endColor.a)
+            rstep = int(diffred / width)
+            gstep = int(diffgreen / width)
+            bstep = int(diffblue / width)
+            astep = int(diffalpha / width)
+            for line in range(0, int(width*scalar)):
+              if (gradColor.r + rstep > 255) or (gradColor.a + astep > 255):
+                pass
+                #break
+              pygame.draw.rect(screen, gradColor, pygame.Rect((x+line)*scalar, y*scalar, scalar, height*scalar))
+              pygame.display.flip()
+              if endColor.r > currentColor.r:
+                gradColor.r += abs(rstep)
+              else:
+                gradColor.r -= rstep
+              
+              if endColor.g > currentColor.g:
+                gradColor.g += abs(gstep)
+              else:
+                gradColor.g -= gstep
+              
+              if endColor.b > currentColor.b:
+                gradColor.b += abs(bstep)
+              else:
+                gradColor.b -= bstep
+
+              if endColor.a > currentColor.a:
+                gradColor.a += abs(astep)
+              else:
+                gradColor.a -= astep
+              
+              
+          elif direction == 'down':
+            raise NotImplementedError
+          elif direction == 'circle':
+            raise NotImplementedError
+        except IndexError:
+          raise NotImplementedError
       elif line[0:7] == "CIRCLE:":
         cleanline = line.replace("(", "").replace(")", "").replace(" ", "").replace("CIRCLE:", "")
         datavals = cleanline.split(",")
         x = int(datavals[0])
         y = int(datavals[1])
         radius = int(datavals[2])
-        pygame.draw.circle(screen, currentColor, (x, y), radius)
+        pygame.draw.circle(screen, currentColor, (x, y), radius*scalar)
       
   pygame.display.flip()
-  sleep(.05)
-
+  #sleep(.05)
+print("Done. Quit to save")
 done = False
 
 while not done:
--- GEL-Converter.py
+++ GEL-Converter.py
@@ -5,7 +5,9 @@ from time import sleep
 converttype = 'lines'
 pygame.init()
 filename = input("Enter filename: ")
-screen = pygame.display.set_mode((int(input("Enter Image Width: ")), int(input("Enter Image Height: "))))
+width = int(input("Enter Image Width: "))
+height = int(input("Enter Image Height: "))
+screen = pygame.display.set_mode((width, height))
 image = pygame.image.load(filename).convert()
 screen.blit(image, (0, 0))
 pygame.display.flip()
@@ -30,8 +32,10 @@ elif converttype == 'lines': #This will be smaller, but still at least image wid
     currentHeight = 1
     currentX = 0
     currentY = 0
+    generatedText += "SIZE: " + str(width) + ", " + str(height) + "\n"
     for column in range(0, len(pixels)):
         for row in range(0, len(pixels[column])):
+            events = pygame.event.get()
             newColor = getRGBfromI(pixels[column][row])
             if newColor == currentColor:
                 currentHeight += 1
@@ -48,7 +52,30 @@ elif converttype == 'lines': #This will be smaller, but still at least image wid
                 pygame.display.flip()
                 #sleep(.05)
 
+
+#fh = open(filename.split('.')[0] + '.txt', 'w')
+#fh.write(generatedText)
+#fh.close()
+textlines = generatedText.split("\n")
+savedLines = {}
+trimtext = "SIZE: " + str(width) + ", " + str(height) + "\n"
+trimRGB = '255,255,255'
+for line in range(0, len(textlines)):
+    if textlines[line][0:4] == 'RGB:':
+        trimRGB = textlines[line]
+    elif textlines[line][0:5] == 'RECT:':
+        try:
+            savedLines[trimRGB] += textlines[line] + "\n"
+        except KeyError:
+            savedLines[trimRGB] = ''
+            savedLines[trimRGB] += textlines[line] + "\n"
+
+savedLines['RGB: 255, 255, 255'] = '' #Should remove all white
+for color in savedLines.items():
+    trimtext += color[0] + "\n"
+    trimtext += color[1]
+trimtext = trimtext.replace("RGB: 255, 255, 255", "") #Removes spurious RGB call
 fh = open(filename.split('.')[0] + '.txt', 'w')
-fh.write(generatedText)
+fh.write(trimtext)
 fh.close()
 exit()
